ada94c096d33d25cb5bc34614341564814da448e
vani123r <90778412+vani123r@users.noreply.github.com>
2023-03-01 12:36:13 -0600

    made all buttons

--- app/src/main/java/com/example/matchu/Questionare.java
+++ app/src/main/java/com/example/matchu/Questionare.java
@@ -2,7 +2,9 @@ package com.example.matchu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Questionare extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Questionare extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionare);
+    }
+    public void stepTwo(View view) {
+        Intent intent = new Intent(Questionare.this, Swipe.class);
+        startActivity(intent);
     }
 }
