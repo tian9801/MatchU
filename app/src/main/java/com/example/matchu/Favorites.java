f6b8286384eb7184625b3543e953adbeca5ce1ff
vani123r <90778412+vani123r@users.noreply.github.com>
2023-03-02 12:54:44 -0600

    nav bar work

--- app/src/main/java/com/example/matchu/Favorites.java
+++ app/src/main/java/com/example/matchu/Favorites.java
@@ -4,12 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.matchu.databinding.ActivityFavoritesBinding;
+
 public class Favorites extends AppCompatActivity {
 
+    ActivityFavoritesBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityFavoritesBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_favorites);
+        binding.bottomNavigationView.setOnClickListener(item -> {
+
+           switch (item.getItemId()){
+               case R.id.favorites:
+
+           }
+            return true;
+        });
     }
 }
