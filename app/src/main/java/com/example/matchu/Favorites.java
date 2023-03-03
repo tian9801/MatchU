7d434ba20d8b96d9abbc597e09e1a1ab6f56c792
vani123r <90778412+vani123r@users.noreply.github.com>
2023-03-03 12:54:07 -0600

    Revert "nav bar"
    This reverts commit c7edaaaf696a1943f4e6085e50ffe7b02ed3f35b.

--- app/src/main/java/com/example/matchu/Favorites.java
+++ app/src/main/java/com/example/matchu/Favorites.java
@@ -1,58 +1,27 @@
 package com.example.matchu;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.example.matchu.databinding.ActivityFavoritesBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Favorites extends AppCompatActivity {
 
-    BottomNavigationView nav;
+    ActivityFavoritesBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityFavoritesBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_favorites);
+        binding.bottomNavigationView.setOnClickListener(item -> {
 
+           switch (item.getItemId()){
+               case R.id.favorites:
 
-        nav = findViewById(R.id.bottomNavigationView);
-        nav.setSelectedItemId(R.id.favorites);
-
-        nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()) {
-                    case R.id.calender:
-                  startActivity(new Intent(getApplicationContext(),Calendar.class));
-                  overridePendingTransition(0,0);
-                  return true;
-
-                    case R.id.favorites:
-                        return true;
-                    case R.id.settings:
-                        startActivity(new Intent(getApplicationContext(),Settings.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.search:
-                        startActivity(new Intent(getApplicationContext(),Serch.class));
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.swipe:
-                        startActivity(new Intent(getApplicationContext(),Swipe.class));
-                        overridePendingTransition(0,0);
-                        return true;
-
-                }
-                return false;
-            }
-
+           }
+            return true;
         });
-
     }
 }
