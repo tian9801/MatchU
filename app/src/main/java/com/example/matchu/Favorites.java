db7e3ccc7c8e69a4026b5f8509557318b2a1afd5
vani123r <90778412+vani123r@users.noreply.github.com>
2023-03-07 01:36:19 -0600

    nav bar works

--- app/src/main/java/com/example/matchu/Favorites.java
+++ app/src/main/java/com/example/matchu/Favorites.java
@@ -1,15 +1,57 @@
 package com.example.matchu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Favorites extends AppCompatActivity {
 
+    BottomNavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+
+        nav = findViewById(R.id.bottomNavigationView);
+        nav.setSelectedItemId(R.id.favorites);
+
+        nav.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.calender:
+                        startActivity(new Intent(getApplicationContext(),Calendar.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.favorites:
+                        return true;
+                    case R.id.settings:
+                        startActivity(new Intent(getApplicationContext(),Settings.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.search:
+                        startActivity(new Intent(getApplicationContext(),Serch.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.swipe:
+                        startActivity(new Intent(getApplicationContext(),Swipe.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+
+        });
+
     }
 }
--- app/src/main/java/com/example/matchu/Serch.java
+++ app/src/main/java/com/example/matchu/Serch.java
@@ -1,14 +1,53 @@
 package com.example.matchu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Serch extends AppCompatActivity {
 
+    BottomNavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serch);
+        nav = findViewById(R.id.bottomNavigationView);
+        nav.setSelectedItemId(R.id.search);
+
+        nav.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.calender:
+                        startActivity(new Intent(getApplicationContext(), Calendar.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.favorites:
+                        startActivity(new Intent(getApplicationContext(), Favorites.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.settings:
+                        startActivity(new Intent(getApplicationContext(), Settings.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.search:
+                        return true;
+
+                    case R.id.swipe:
+                        startActivity(new Intent(getApplicationContext(), Swipe.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                }
+                return false;
+            }
+
+        });
     }
 }
