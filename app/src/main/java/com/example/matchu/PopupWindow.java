fd421b00229aea7baa8644dfe88b8dac12180dba
tian9801 <90787165+tian9801@users.noreply.github.com>
2023-04-21 12:55:19 -0500

    started implementing popupwindow (it crashes)

---
+++ app/src/main/java/com/example/matchu/PopupWindow.java
@@ -0,0 +1,54 @@
+package com.example.matchu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.PopupWindow;
+
+public class PopupWindow extends AppCompatActivity {
+
+    RelativeLayout parent;
+
+    public PopupWindow(View view, int width, int height, boolean b) {
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_window);
+
+        parent = findViewById((R.id.parent_layout));
+        Button show = findViewById(R.id.show_pop);
+
+        show.setOnClickListener((v -> {
+            showPopupWindow();
+        }));
+    }
+
+
+    private void showPopupWindow(){
+        View view = View.inflate(this, R.layout.popup_layout, null);
+        Button close = view.findViewById(R.id.close);
+        TextView body = view.findViewById(R.id.popup_msg);
+
+
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        PopupWindow popupWindow = new PopupWindow(view, width, height, false);
+
+        popupWindow.showAtLocation(parent, Gravity.CENTER, 0,0);
+
+        body.setText(R.string.message);
+        close.setOnClickListener(v -> {
+            popupWindow.dismiss();
+
+        });
+    }
+}
