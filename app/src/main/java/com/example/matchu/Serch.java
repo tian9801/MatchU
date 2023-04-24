b725fec63b7a119ae415772d2bf3e176691658f3
ShreyaSridhar04 <90780275+ShreyaSridhar04@users.noreply.github.com>
2023-04-24 12:39:16 -0500

    dummy listview for search

--- app/src/main/java/com/example/matchu/Serch.java
+++ app/src/main/java/com/example/matchu/Serch.java
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Serch extends AppCompatActivity {
+
+    //when this dummy list works, use the collegesDB from the other class
+    ListView listView;
+    String names[] = {"Chris", "Emma", "Jack", "Shreya", "Liana"};
+
+    ArrayAdapter<String> arrayAdapter;
 
     BottomNavigationView nav;
     @Override
@@ -49,5 +59,34 @@ public class Serch extends AppCompatActivity {
             }
 
         });
+
+        listView = findViewById(R.id.listviewcollege);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.serchpage, menu);
+        MenuItem collegeItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) collegeItem.getActionView();
+        searchView.setQueryHint("Type here to search a college");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            //this method is called when the user types in something and enters
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            //this method is called when the user types in a new character or makes any changes while typing
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
