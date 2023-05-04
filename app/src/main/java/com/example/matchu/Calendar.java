8a6889e3be6f2d7af7b8af4077dfa79044ae4d70
ShreyaSridhar04 <90780275+ShreyaSridhar04@users.noreply.github.com>
2023-05-04 12:43:28 -0500

    CALENDAR is working

--- app/src/main/java/com/example/matchu/Calendar.java
+++ app/src/main/java/com/example/matchu/Calendar.java
@@ -1,5 +1,8 @@
 package com.example.matchu;
 
+import static com.example.matchu.CalendarUtils.daysInMonthArray;
+import static com.example.matchu.CalendarUtils.monthYearfromDate;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,8 +21,6 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Calendar extends AppCompatActivity implements CalendarAdapter.OnItemListener {
@@ -27,7 +28,7 @@ public class Calendar extends AppCompatActivity implements CalendarAdapter.OnIte
     BottomNavigationView nav;
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
-    private LocalDate selectedDate;
+
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -36,10 +37,12 @@ public class Calendar extends AppCompatActivity implements CalendarAdapter.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         initWidgets();
-        selectedDate = LocalDate.now();
+        CalendarUtils.selectedDate = LocalDate.now();
         setMonthView();
 
-        
+
+
+
         nav = findViewById(R.id.bottomNavigationView);
         nav.setSelectedItemId(R.id.calender);
 
@@ -84,8 +87,8 @@ public class Calendar extends AppCompatActivity implements CalendarAdapter.OnIte
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void setMonthView(){
-        monthYearText.setText(monthYearfromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+        monthYearText.setText(monthYearfromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
         CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth,this);
         RecyclerView.LayoutManager  layoutManager = new GridLayoutManager(getApplicationContext(),7);
         calendarRecyclerView.setLayoutManager(layoutManager);
@@ -95,37 +98,11 @@ public class Calendar extends AppCompatActivity implements CalendarAdapter.OnIte
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private ArrayList<String> daysInMonthArray (LocalDate date) {
-        ArrayList<String> daysInMonthArray = new ArrayList<>();
-        YearMonth yearMonth = YearMonth.from(date);
-        int daysInMonth = yearMonth.lengthOfMonth();
-        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
-        int daysOfWeek = firstOfMonth.getDayOfWeek().getValue();
 
-        for (int i = 1; i <= 42; i++) {
-
-            if(i <= daysOfWeek || i > daysInMonth + daysOfWeek){
-                daysInMonthArray.add("");
-
-            }else{
-                daysInMonthArray.add(String.valueOf(i - daysOfWeek));
-
-            }
-
-            
-
-
-        }
-        
-        return daysInMonthArray;
-
-
-    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void previousMonthAction(View view){
-        selectedDate = selectedDate.minusMonths(1);
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
         setMonthView();
 
 
@@ -134,7 +111,7 @@ public class Calendar extends AppCompatActivity implements CalendarAdapter.OnIte
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void nextMonthAction(View view){
-        selectedDate = selectedDate.plusMonths(1);
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
         setMonthView();
 
 
@@ -145,27 +122,25 @@ public class Calendar extends AppCompatActivity implements CalendarAdapter.OnIte
 
 
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private String monthYearfromDate(LocalDate date){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-        return date.format(formatter);
 
-
-    }
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    public void onItemClick(int position, String dayText) {
-        if(!dayText.equals("")){
-            Log.i("what", " " + monthYearfromDate(selectedDate));
-            String message = "Selected Date " + dayText + " " + monthYearfromDate(selectedDate);
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    public void onItemClick(int position, LocalDate date) {
+        if(date != null){
+            CalendarUtils.selectedDate = date;
+            setMonthView();
+
 
         }
 
     }
 
 
-
+    public void weeklyAction(View view) {
+        startActivity(new Intent(this, WeekViewActivity.class));
+    }
 }
+
+
